e9ba02d7f58893ebdae9be99e8a54bea93ccf101
Oliver <soliver@usgs.gov>
2018-01-12 08:39:44 -0600

    use `pah` function `plot_pah` to create a concentration by watershed bar chart, with color coding by lake.

--- 4_summarize_data/src/summarize_data.R
+++ 4_summarize_data/src/summarize_data.R
@@ -1,3 +1,19 @@
-conc_by_site <- function() {
+conc_by_site <- function(samples, fig_file_path) {
+  df <- samples
+  df$unique_id <- paste0(df$State, "-", df$STAT_ID)
   
+  df <- filter(df, PARAM_SYNONYM %in% "Priority Pollutant PAH") %>%
+    select(unique_id, RESULT, State, Watershed, Lake, PARAM_SYNONYM) %>%
+    group_by(unique_id, State, Watershed, Lake, PARAM_SYNONYM) %>%
+    summarize(RESULT = mean(RESULT))
+  
+  p <- pah::plot_pah(pah_dat = df, conc_column = 'RESULT', sample_id_column = 'unique_id',
+                compound_column = 'PARAM_SYNONYM', compound_plot = "Priority Pollutant PAH",
+                color_column = 'Lake', group_column = 'Watershed', conc_units = 'ppb')
+  
+  ggplot2::ggsave(fig_file_path, 
+         p, height = 6, width = 14)
+  
+  # had to add "PEC" and "TEC" labels manually because
+  # facets weren't wide enough to accomodate the text.
 }
