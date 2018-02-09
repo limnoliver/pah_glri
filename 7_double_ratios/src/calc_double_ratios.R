7be8879541c5e44f7bb734762f129fa2e8aafde7
Oliver <soliver@usgs.gov>
2018-02-09 11:59:49 -0600

    double ratio figs

--- 7_double_ratios/src/calc_double_ratios.R
+++ 7_double_ratios/src/calc_double_ratios.R
@@ -1,16 +1,16 @@
-library(dplyr)
-library(tidyr)
-# create double ratio plots
+make_ratios <- function(pah_dat = samples){
+  #pah_dat <- make('samples')
+  pah_dat$sample_id <- paste0(pah_dat$State, "-", pah_dat$STAT_ID)
+  pah_dat <- select(pah_dat, sample_id, casrn, PARAM_SYNONYM, RESULT) %>%
+    group_by(sample_id, PARAM_SYNONYM, casrn) %>%
+    summarize(RESULT = mean(RESULT)) %>%
+    ungroup()
+  
+  ratios <- calc_ratios(pah_dat, sample_column = 'sample_id', conc_column = 'RESULT')
+  return(ratios)
+}
 
-# list of arguments
-# 
-
-pah_dat <- make('samples')
-pah_dat$sample_id <- paste0(pah_dat$State, "-", pah_dat$STAT_ID)
-pah_dat <- select(pah_dat, sample_id, casrn, PARAM_SYNONYM, RESULT) %>%
-  group_by(sample_id, PARAM_SYNONYM, casrn) %>%
-  summarize(RESULT = mean(RESULT)) %>%
-  ungroup()
-
-ratios <- calc_ratios(pah_dat, sample_column = 'sample_id', conc_column = 'RESULT')
-
+ratio_plot <- function(filename) {
+  p <- plot_ratios(ratios)
+  ggsave(filename, p, height = 5, width = 16)
+}
