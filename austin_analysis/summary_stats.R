b1be696b9c32be2634619994a81416f38af522a4
Oliver <soliver@usgs.gov>
2018-03-26 09:17:27 -0500

    added major remake targets for each step in PAH analysis workflow. This can be used by Austin to quickly find data he might be looking for

--- austin_analysis/summary_stats.R
+++ austin_analysis/summary_stats.R
@@ -1,5 +1,110 @@
-#
-pah16 <- make('prepped_totals')
+# Documentation of various steps in the PAH analysis
+# and how/where to get the appropriate data and figures
+# you may need.
 
-head(pah16)
-mean.val <- mean(pah16$Priority16)
+##################################################
+######### get sample concentrations ##############
+# target "samples" is all of the concentration data
+# that has been merged with compound-specific data
+# (e.g., low vs high molecular weight). BDLs have been
+# converted to zero, and raw concentration data has been merged
+# with compound-specific data (e.g., molecular weight) using function ?pah::get_compound_info
+# compound metadata stored in pah package pah::pah_compounds
+
+samples <- make('samples') # just samples, no blanks or duplicates
+# each row is a different sample-compound combination (so a long table)
+# units are ppb
+# site and compound metadata columns at end of table
+pah16 <- make('prepped_totals') # sample ID's and corresponding EPA16 concentration
+site_order <- make('sample_order_pah16') # a list of site ID's, from lowest to highest EPA16 concentration
+
+########################################
+######### QA data ##################
+# these targets were made in 3_filter_data
+blanks <- make('blanks') # blanks with coresponding samples and duplicates
+duplicates <- make('duplicates') # duplicates with corresponding samples
+
+# summary QA targets were made in 10_qa_summary
+
+# for each compound, blank summary statistics were calculated:
+# n_FB_bdl: the number of instances where the blank was below detection limit
+# n_both_bdl: the number of times the blank and corresponding sample were BDL
+# n_SA_bdl: the number of times the corresponding sample was BDL, but not the blank
+# n_both_adl: the number of times both the blank and corresponding sample were above
+# detection limit. These were the instances that were analyzed for percent differences
+# mean: mean of all percent difference between blank and sample (when both were > DL)
+# medain: as above
+# stdev: as above
+# min: as above
+# max: as above
+qa_blanks <- make('qa_blanks') 
+
+# for each duplicate sample, summary statistics were calculated relative to the sample.
+# relative percent differences were calculated for each sample-compound combination, 
+# and then for each compound, summary statistics were calculated:
+# mean: for each compound, mean of all RPD between duplicate and sample
+# median, stdev, min, max: as above
+# n_both_bdl: number of samples where both duplicate and sample were BDL
+# n_one_bdl: number of samples where one but not both sample and duplicate were BDL
+qa_duplicates <- make('qa_duplicates')
+
+####################################
+########## profiles ################
+# profile dat, list of 2
+# part 1: long format for profiles, where each row is a unique site-compound-source combination
+# and both sample and source proportional concentration have their own columns
+# part 2: sum chi2 for each sample-source combination
+
+profiles <- make('profiles')
+
+#################################
+######## PCA anaysis ############
+# summary stats of PCA are output when the pah::pah_pca
+# function is run, but can also be accessed in the output
+# pah_pca lets you change selection criteria for PCA components,
+# but in this case, we kept all components that explained >= 10% variation
+
+pca_dat <- make('pca_no_creosote')
+
+# PCA stats output
+pca_dat$pca_summary # first 4 components all explained > 10%, together explained 78% of total variance
+
+# raw PCA output of selected components
+# output in long format, where each row is a sample or source, and column "type" 
+# tells you whether it is a sample or source value
+raw_pca <- pca_dat$pca_dat
+
+# pca distances, long format (each row is unique sample-source combo)
+# gives euclidean distances in all selected PCA component space
+pca_dist <- pca_dat$pca_distance
+
+# closest source by site using PCA distance
+top_pca <- make('pca_top_sources_bysite')
+
+###################################
+########## double ratios ##########
+
+# raw ratios for source and samples
+# uses pah::source_ratios and pah::calc_ratios
+# Each ratio is its own column, and sources and samples are both in "sample_id"
+# column but are distinguished by the "sample_type" column.
+ratios <- make('ratios')
+
+# calculated distances for each double ratio plot using pah::cal_ratio_dist
+# list of 3: raw (raw distances for each source-sample-double ratio plot combo), source (raw distance
+# data summarized by source), and samples (top sources for each sample calculated in each double ratio plot)
+# see 7_double_ratios.yml for more descriptions
+ratio_dist <- make('ratio_distance')
+
+###################################
+#### parent vs alkyl and ESBTU ####
+
+# the compound metadata to calculate these are already in 
+# the "samples" target and come from pah::pah_compounds
+# The data could be retrieved by using pah::pah_mw_parent function and 
+# setting plot = FALSE which will output a data frame
+# see 9_parent_weight.yml for how this function is used
+
+####################################
+####
+
