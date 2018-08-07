77233fde359d62a915400436afadfff9c0a89532
Samantha Oliver <soliver@usgs.gov>
2018-08-07 09:38:12 -0500

    changed to using the unique_id that is generated earlier in workflow

--- 7_double_ratios/src/calc_double_ratios.R
+++ 7_double_ratios/src/calc_double_ratios.R
@@ -1,12 +1,12 @@
 make_ratios <- function(pah_dat = samples){
   #pah_dat <- make('samples')
-  pah_dat$sample_id <- paste0(pah_dat$State, "-", pah_dat$STAT_ID)
-  pah_dat <- select(pah_dat, sample_id, casrn, PARAM_SYNONYM, RESULT) %>%
-    group_by(sample_id, PARAM_SYNONYM, casrn) %>%
+  #pah_dat$sample_id <- paste0(pah_dat$State, "-", pah_dat$STAT_ID)
+  pah_dat <- select(pah_dat, unique_id, casrn, PARAM_SYNONYM, RESULT) %>%
+    group_by(unique_id, PARAM_SYNONYM, casrn) %>%
     summarize(RESULT = mean(RESULT)) %>%
     ungroup()
   
-  ratios <- calc_ratios(pah_dat, sample_column = 'sample_id', conc_column = 'RESULT')
+  ratios <- calc_ratios(pah_dat, sample_column = 'unique_id', conc_column = 'RESULT')
   return(ratios)
 }
 
