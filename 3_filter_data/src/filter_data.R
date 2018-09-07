94448124b19893af505e63b7a71d772a0d274a55
Samantha Oliver <soliver@usgs.gov>
2018-09-07 11:02:01 -0500

    remove making a column unique_id - already done upstream

--- 3_filter_data/src/filter_data.R
+++ 3_filter_data/src/filter_data.R
@@ -1,9 +1,7 @@
 # return blanks and corresponding samples with same state/site id/date
 # this will return samples, duplicates, and blanks for sites with blanks
 filter_blanks <- function(processed_sample) {
-  # create a unique id for state/site/date combination
-  processed_sample$unique_id <- paste(processed_sample$State, processed_sample$STAT_ID, processed_sample$COLLECTION_DATE_NOTIME, sep = "_")
-  
+
   # find blanks and get the unique id of all blanks
   blank_sample <- filter(processed_sample, processed_sample$FIELD_QC_CODE == "FB")
   blank_sample_ids <- unique(blank_sample$unique_id)
@@ -18,8 +16,7 @@ filter_blanks <- function(processed_sample) {
 # this will return duplicates and corresponding samples from the same state/site/date
 
 filter_duplicates <- function(processed_sample) {
-  processed_sample$unique_id <- paste(processed_sample$State, processed_sample$STAT_ID, processed_sample$COLLECTION_DATE_NOTIME, sep = "_")
-  
+
   # find duplicates and get the unique id of all duplicates
   duplicate_sample <- filter(processed_sample, processed_sample$FIELD_QC_CODE == "DU")
   duplicate_sample_ids <- unique(duplicate_sample$unique_id)
